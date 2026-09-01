d0c67d8967648b6f28f780960924df1ac414fd25
mishael garrett (Mishgamon) <mishga@umich.edu>
2026-09-01 12:50:04 -0400

    Added an AddScore and RollDice function, as well as program display_rules and play_game

--- PigDice.cpp
+++ PigDice.cpp
@@ -11,6 +11,62 @@ struct GameState {
     bool turn_over = false;
 };
 
+void addscore(GameState &gs) {
+    gs.game_score += 10;
+}
+
+void display_rules() {
+    std::cout << "Let's Play PIG Dice!\n * See how many turns it takes you to get to 20 points.\n * Turn ends when you hold or roll a 1.\n * If you roll a 1, you lose all points for the turn.\n * If you hold, you bank all points for the turn to the game score. \n";
+
+
+}
+
+int roll_dice() {
+    int x = (rand() % 6)+1;
+    std::cout << "Die: " << x;
+    return x;
+}
+
+void play_game(GameState &game) {
+    while (!game.game_over) {
+    game.turn_count++;
+    std::cout << "\nTURN " << game.turn_count << " - Game Score: " << game.game_score;
+game.score_this_turn=0;
+        while (game.choice != 'h') {
+            std::cout << "\nroll or hold? (r/h): ";
+            std::cin >>game.choice;
+        if (game.choice == 'r') {
+            int x = roll_dice();
+             if (x==1) {
+                 game.choice='h';
+                 game.score_this_turn=0;
+                 std::cout << "\nTurn over.  No score.";
+             }else {
+                 game.score_this_turn+=x;
+                 std::cout<<" - Running score this turn: "<<game.score_this_turn;
+             }
+        }else if (game.choice!='h'){
+            std::cout << "\nInvalid Entry.  Please use 'r' for ''roll'' or 'h' for ''hold''.";
+        }
+
+
+    }
+        std::cout<<"\nScore banked this turn: "<<game.score_this_turn;
+        game.game_score+=game.score_this_turn;
+game.choice=' ';
+        std::cout << "\n";
+        if (game.game_score>=20)game.game_over=true;
+}
+
+std::cout << "\nYou finished with a final score of "<<game.game_score<<" in "<<game.turn_count<<" turn";
+if (game.turn_count!=1)std::cout << "s";
+    std::cout<<"!\nThanks for playing PIG dice!";
+
+}
+
+
+
+
 int main() {
     GameState my_game; // instantiate a GameState object
     display_rules(); // call the display_rules function
